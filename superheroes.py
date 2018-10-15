094adde6a175e00b183e0233823527da6ac11b48
Rinni Swift <rinradaswift@gmail.com>
2018-10-15 16:11:45 -0700

    updated complete project

--- superheroes.py
+++ superheroes.py
@@ -7,10 +7,9 @@ class Ability:
 		self.attack_strength = attack_strength
 
 	def attack(self):
-		lowest_attack = self.attack_strength // 2
-		attack = random.randint(lowest_attack, self.attack_strength)
-		return attack
-
+		lowest_attack_value = int(self.attack_strength) // 2
+		attack_value = random.randint(lowest_attack_value, int(self.attack_strength))
+		return attack_value
 
 
 	def update_attack(self, attack_strength):
@@ -19,8 +18,7 @@ class Ability:
 
 class Weapon(Ability):
 	def attack(self):
-		attac = random.randint(0, self.attack_strength)
-		return attac
+		return random.randint(0, self.attack_strength)
 
 
 class Armor:
@@ -30,8 +28,8 @@ class Armor:
 		self.defense = defense
 
 	def defend(self):
-		random_value = random.randint(0, self.defense)
-		return random_value
+		random_defend_value = random.randint(0, int(self.defense))
+		return random_defend_value
 		
 
 class Hero:
@@ -39,7 +37,6 @@ class Hero:
 	def __init__(self, name, health=100):
 		self.abilities = list()
 		self.name = name
-
 		self.armors = list()
 		self.start_health = health
 		self.health = health
@@ -50,29 +47,27 @@ class Hero:
 		return self.name
 
 	def defend(self):
-		total_defense = 0
 
+		defenses = 0
 		if len(self.armors) == 0:
 			return 0
 
-		for i in self.armors:
-			total_defense += i.defend()
+		for armor in self.armors:
+			defenses += armor.defend()
 
 		if self.health == 0:
-			total_defense = 0
-			return total_defense
+			defenses = 0
+			return defenses
 		else:
-			return total_defense
+			return defenses
+
 
 	def add_armor(self, name):
 		self.armors.append(name)
 
-
-
-
-
 	def take_damage(self, damage_amt):
 		self.health = self.health - damage_amt
+
 		if self.health <= 0:
 			self.deaths += 1
 		else:
@@ -81,20 +76,14 @@ class Hero:
 	def add_kill(self, num_kills):
 		self.kills += num_kills
 
-
-		
-
-	def __str__(self):
-		return self.name
-
 	def add_ability(self, ability):
 		self.abilities.append(ability)
 
 	def attack(self):
-		total_attacks = 0
-		for i in self.abilities:
-			total_attacks += i.attack()
-		return total_attacks
+		total = 0
+		for x in self.abilities:
+			total += x.attack()
+		return total
 
 
 class Team():
@@ -111,65 +100,68 @@ class Team():
 		if len(self.heroes) == 0:
 			return 0
 
-		for i in self.heroes:
-			print(i.name)
-			if i.name == name:
-				self.heroes.remove(i)
+		for hero in self.heroes:
+			if hero.name == name:
+				self.heroes.remove(hero)
+				return True
 			else:
 				return 0
+
+
 
 	def find_hero(self, name):
 		if len(self.heroes) == 0:
 			return 0
-
 		for i in self.heroes:
 			if i.name == name:
-				return i
-			else:
-				return 0
+				return hero
+		return 0
 
 	def view_all_heroes(self):
+
+		viewed_heroes = []
+		if len(self.heroes) == 0:
+			print("No heroes in this team!")
 		for hero in self.heroes:
-			print(hero)
+			viewed_heroes.append(hero.name)
+		print(viewed_heroes)
+
 
 
 	def attack(self, other_team):
-		total_attack_strength = 0
+		tot_attack_strength = 0
 		for hero in self.heroes:
-			total_attack_strength += hero.attack()
-			killed_heroes = other_team.defend(total_attack_strength)
-
-
+			tot_attack_strength += hero.attack()
+		killed_heroes = other_team.defend(tot_attack_strength)
 
 		for hero in self.heroes:
 			hero.add_kill(killed_heroes)
 
-	def defend(self, damage_amt):
-		total_defense = 0
+	def defend(self, total_attack_strength):
+		tot_defence = 0
 		for hero in self.heroes:
-			total_defense += hero.defend()
-
-		if damage_amt > total_defense:
-			excess_damage = damage_amt - total_defense
-			num_kills = self.deal_damage(excess_damage)
-		return num_kills
+			tot_defence += hero.defend()
+		excess_damage = total_attack_strength - tot_defence
+		if excess_damage > 0:
+			return self.deal_damage(excess_damage)
+		else:
+			return 0
 
 
 	def deal_damage(self, damage):
 
 		if len(self.heroes) != 0:
-			individual_damage = damage // len(self.heroes)
+			damage_per_hero = damage // len(self.heroes)
 		else:
 			return 0
-
-		dead_heroes = 0
+		count = 0
 		for hero in self.heroes:
-			if hero.health > individual_damage:
-				hero.take_damage(individual_damage)
+			if hero.health > damage_per_hero:
+				hero.take_damage(damage_per_hero)
 			else:
-				hero.take_damage(individual_damage)
-				dead_heroes += 1
-		return dead_heroes
+				hero.take_damage(damage_per_hero)
+				count += 1
+		return count
 
 
 
@@ -177,16 +169,12 @@ class Team():
 
 	def revive_heroes(self, health=100):
 		for hero in self.heroes:
-			hero.health = hero.start_health
+			hero.health = 100
 
 
 	def stats(self):
-		# for hero in self.heroes:
-		# 	kills = hero.killed 
-		# 	deaths = hero.deaths
-		# 	print("ratio of kills to deaths is {}:{}".format(kills, deaths))
-
-		print("ratio of kills to deaths is {}:{}".format(self.kills, self.deaths))
+		for hero in self.heroes:
+			print("{}'s ratio of kills to deaths is {}:{}".format(hero.name, hero.kills, hero.deaths))
 
 	def update_kills(self):
 		for hero in self.heroes:
@@ -201,10 +189,62 @@ class Arena:
 
 	def build_team_one(self):
 		self.team_one = Team(input("Input team one name: "))
+		print("Go {}!!!".format(self.team_one.team_name))
+		print("\n")
+		hero1 = Hero(input("input a hero's name: "))
+		self.team_one.add_hero(hero1)
+		print("You have added {} to {}".format(hero1.name, self.team_one.team_name))
+		
+		print("\n")
+		ability1 = Ability(input("input name of ability: "), input("input ability's attack strength: "))
+		hero1.add_ability(ability1)
+		print("You have added the ability {} to {}".format(ability1.name, hero1.name))
+		
+		print("\n\n")
+		hero2 = Hero(input("input a second hero's name: "))
+		self.team_one.add_hero(hero2)
+		print("You have added {} to {}".format(hero2.name, self.team_one.team_name))
+
+		print("\n")
+		ability2 = Ability(input("input name of ability: "), input("input ability's attack strength: "))
+		hero2.add_ability(ability2)
+		print("You have added the ability {} to {}".format(ability2.name, hero2.name))
 
 
 	def build_team_two(self):
 		self.team_two = Team(input("Input team two name: "))
+		print("Go {}!!!".format(self.team_two.team_name))
+        # add the hero
+		print("\n")
+		hero1 = Hero(input("input a hero's name: "))
+		self.team_two.add_hero(hero1)
+		print("You have added {} to {}".format(hero1.name, self.team_two.team_name))
+        # add the ability
+		print("\n")
+		ability1 = Ability(input("input name of ability: "), input("input ability's attack strength: "))
+		hero1.add_ability(ability1)
+		print("You have added the ability {} to {}".format(ability1.name, hero1.name))
+        # add armor
+		print("\n")
+		armor = Armor(input("input name of armor: "), input("input armors attack strenght: "))
+		hero1.add_armor(armor)
+		print("You have added {} to {}".format(armor.name, hero1.name))
+
+        # add hero 2
+		print("\n\n")
+		hero2 = Hero(input("input a second hero's name: "))
+		self.team_two.add_hero(hero2)
+		print("You have added {} to {}".format(hero2.name, self.team_two.team_name))
+        # add ability to hero 2
+		print("\n")
+		ability2 = Ability(input("input name of ability: "), input("input ability's attack strength: "))
+		hero2.add_ability(ability2)
+		print("You have added the ability {} to {}".format(ability2.name, hero2.name))
+        # add armor to hero 2
+		print("\n")
+		armor2 = Armor(input("input name of armor: "), input("input armors attack strenght: "))
+		hero2.add_armor(armor2)
+		print("You have added {} to {}".format(armor2.name, hero2.name))
 
 	def team_battle(self):
 		running = True
@@ -221,18 +261,18 @@ class Arena:
 			for hero in self.team_two.heroes:
 				if hero.health <= 0:
 					deaths_team_two += 1
-
 			self.team_two.attack(self.team_one)
 			if deaths_team_two == len(self.team_two.heroes):
 				running = False
 
 	def show_stats(self):
-		for hero in self.team_one:
-			print("{} kill to death ratio is {}:{}".format(self.name, self.kills, self.deaths))
+		print("\n")
+		print("Team 1 stats:")
+		self.team_one.stats()
 
-		for hero in self.team_two:
-			print("{} kill to death ratio is {}:{}".format(self.name, self.kills, self.deaths))
-
+		print("\n")
+		print("Team 2 stats:")
+		self.team_two.stats()
 
 
 
